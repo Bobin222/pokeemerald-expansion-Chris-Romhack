986e520ca6bb2d4db6fa22d247319d91b911a211
Bobin222 <boothmatt20@gmail.com>
2026-09-11 14:14:06 -0400

    Adding hidden stones to stevens room in granite cave

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1390,10 +1390,10 @@
 #define FLAG_HIDE_OLDALE_TOWN_EVOLUTION_MERCHANT    (SYSTEM_FLAGS + 0x21) // Flags for the Oldale Town Merchants
 #define FLAG_HIDE_OLDALE_TOWN_BERRY_MERCHANT        (SYSTEM_FLAGS + 0x22) // Flags for the Oldale Town Merchants
 #define FLAG_HIDE_OLDALE_TOWN_BATTLE_MERCHANT       (SYSTEM_FLAGS + 0x23) // Flags for the Oldale Town Merchants
-#define FLAG_UNUSED_0x884                           (SYSTEM_FLAGS + 0x24) // Unused Flag
-#define FLAG_UNUSED_0x885                           (SYSTEM_FLAGS + 0x25) // Unused Flag
-#define FLAG_UNUSED_0x886                           (SYSTEM_FLAGS + 0x26) // Unused Flag
-#define FLAG_UNUSED_0x887                           (SYSTEM_FLAGS + 0x27) // Unused Flag
+#define FLAG_HIDDEN_ITEM_GRANITE_CAVE_STEVENS_ROOM_FIRE_STONE      (SYSTEM_FLAGS + 0x24) // Flags for Stones hidden in Steven's Room in Granite Cave
+#define FLAG_HIDDEN_ITEM_GRANITE_CAVE_STEVENS_ROOM_WATER_STONE     (SYSTEM_FLAGS + 0x25) // Flags for Stones hidden in Steven's Room in Granite Cave
+#define FLAG_HIDDEN_ITEM_GRANITE_CAVE_STEVENS_ROOM_THUNDER_STONE   (SYSTEM_FLAGS + 0x26) // Flags for Stones hidden in Steven's Room in Granite Cave
+#define FLAG_HIDDEN_ITEM_GRANITE_CAVE_STEVENS_ROOM_ICE_STONE       (SYSTEM_FLAGS + 0x27) // Flags for Stones hidden in Steven's Room in Granite Cave
 
 #define FLAG_SYS_USE_FLASH                          (SYSTEM_FLAGS + 0x28)
 #define FLAG_SYS_USE_STRENGTH                       (SYSTEM_FLAGS + 0x29)
