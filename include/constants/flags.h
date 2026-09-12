de30d267990fb7bd44105b9f9cc237015b87240b
Bobin222 <boothmatt20@gmail.com>
2026-09-12 12:31:27 -0400

    Adding Z-power event in Verdanturf Town with Wally's Uncle after you beat him

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1403,7 +1403,7 @@
 #define FLAG_SYS_SAFARI_MODE                        (SYSTEM_FLAGS + 0x2C)
 #define FLAG_SYS_CRUISE_MODE                        (SYSTEM_FLAGS + 0x2D)
 
-#define FLAG_UNUSED_0x88E                           (SYSTEM_FLAGS + 0x2E) // Unused Flag
+#define FLAG_RECEIVED_Z_POWER_RING_FROM_WALLYS_UNCLE           (SYSTEM_FLAGS + 0x2E) // Unused Flag
 #define FLAG_UNUSED_0x88F                           (SYSTEM_FLAGS + 0x2F) // Unused Flag
 
 #define FLAG_SYS_TV_HOME                            (SYSTEM_FLAGS + 0x30)
